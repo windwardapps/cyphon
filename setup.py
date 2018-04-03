f4de2c9f1716b890923b5bb80835408cbb64226f
Chase Brewer <chase.brewer@dunbarcyber.com>
2018-04-03 10:19:09 -0400

    add version number 1.6.3 in select file

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='cyphon',
-    version='1.6.2',
+    version='1.6.3',
     install_requires=REQUIREMENTS,
     packages=find_packages(),
     include_package_data=True,
